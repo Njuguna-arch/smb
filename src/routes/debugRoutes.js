f9cecb22f6e78830ab3d2fb7aff36dc1284fe128
Joseph <j44gichanga@gmail.com>
2026-04-10 10:20:32 +0300

    updated debugRoutes imports

--- src/routes/debugRoutes.js
+++ src/routes/debugRoutes.js
@@ -1,18 +1,9 @@
 // routes/debugRoutes.js
 import express from "express";
-import {
-  testPDF,
-  testPDFStudentInfo,
-  testPDFTableHeader,
-  testPDFOneRow,
-} from "../controllers/debugController.js";
+import { generateStudentReport } from "../controllers/debugController.js";
 
 const router = express.Router();
 
-// Debug routes (no auth middleware so you can test quickly)
-router.get("/test-pdf", testPDF);
-router.get("/test-student", testPDFStudentInfo);
-router.get("/test-table", testPDFTableHeader);
-router.get("/test-row", testPDFOneRow);
+router.post("/student-report", generateStudentReport);
 
 export default router;
