d8af5367cf64a806fc503c5d84fa4664d89600de
Joseph <j44gichanga@gmail.com>
2026-05-15 18:40:49 +0300

    updated outdated mongoose

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      dbName: "schoolDB",
+      dbName: "schoolDB", 
     });
 
     console.log(
