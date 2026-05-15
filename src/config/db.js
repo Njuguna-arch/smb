dace465dae47b7d5aa4599bd733bad81f58d0a1a
Joseph <j44gichanga@gmail.com>
2026-05-15 18:33:09 +0300

    added dbname in db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,13 +2,17 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: "schoolDB",
+    });
 
     console.log(
-      ` MongoDB connected: ${conn.connection.host} / Database: ${conn.connection.name}`
+      `MongoDB connected: ${conn.connection.host} / Database: ${conn.connection.name}`
     );
   } catch (err) {
-    console.error(" MongoDB connection failed:", err.message);
+    console.error("MongoDB connection failed:", err.message);
     process.exit(1);
   }
 };
